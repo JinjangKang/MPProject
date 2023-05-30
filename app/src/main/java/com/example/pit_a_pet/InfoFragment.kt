2d699b532dd41a9f7f0849b18ca608582706556f
taulmax <taulmax@gmail.com>
2023-05-31 01:36:52 +0900

    [feat] 반려동물 상식 세팅 완료

--- app/src/main/java/com/example/pit_a_pet/InfoFragment.kt
+++ app/src/main/java/com/example/pit_a_pet/InfoFragment.kt
@@ -5,19 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.pit_a_pet.databinding.FragmentInfoBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [InfoFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+private lateinit var binding: FragmentInfoBinding
+
 class InfoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -32,10 +29,15 @@ class InfoFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false)
+    ): View {
+        binding = FragmentInfoBinding.inflate(inflater,container,false)
+
+
+
+
+        return binding.root
     }
+
 
     companion object {
         /**
